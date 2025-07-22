ff22ac511b5f1f81eb724f3f1e80047b43dace90
Cecile <cecile.kim.sakamoto@gmail.com>
2025-07-21 20:00:58 -0700

    Add path aliases for imports, add type definition file for .graphql files to support importing of schema.graphql

--- src/feature/vendor/index.ts
+++ src/feature/vendor/index.ts
@@ -1,28 +1,20 @@
-// feature model exports
-import { VendorResolver } from './graphql/resolvers';
-import { Resolvers } from './graphql/schema';
-import VendorService from './services/vendor.service';
-import VendorModel from './models/vendor.model';
+import VendorResolver from './graphql/resolvers';
+import VendorSchema from './graphql/schema.graphql';
+import { VendorService } from './service';
+import VendorModel from './model';
 
 export default {
-  // GraphQL
   resolvers: VendorResolver,
-  schema: vendorSchema,
+  schema: VendorSchema,
 
-  // Services
   services: {
     vendorService: VendorService,
   },
 
-  // Models
   models: {
     Vendor: VendorModel,
   },
 
-  // REST Controllers
-  controllers: [], // if using REST
-
-  // Constants/Types
   ...require('./constants'),
   ...require('./types'),
 };
